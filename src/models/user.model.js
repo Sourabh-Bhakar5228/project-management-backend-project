5fe28b777de83a1be29944905dce1faab108667e
Sourabh-Bhakar5228 <golubhakar5228@gmail.com>
2025-05-12 11:29:43 +0530

    backend api testing again

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -43,12 +43,12 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-// userSchema.pre('save', async function(next) {
-//     if (this.isModified('password')) {
-//         this.password = await bcrypt.hash(this.password, 10);
-//     }
-//     next();
-// });
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
